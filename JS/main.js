f2787bce8a409c902db51737b1b74f5939cc1132
Laura <laura.dionisi91@gmail.com>
2021-02-23 22:52:57 +0100

    Biglietto treno rivisto e corretto

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,73 @@
-var nome = document.getElementById("nome_cognome").value;
-console.log("nome");
-document.getElementById("nome_cognome").innerHTML = nome;
+// eventi collegati al bottone genera
 
-var numeroKm = document.getElementById("num_km").value;
-console.log("numeroKm");
+var btn_genera = document.getElementById("btn_genera");
 
-var anni = document.getElementById("num_anni").value;
-console.log("anni");
-
-const PREZZOKM = 0.21;
-
-var prezzoBase;
-prezzoBase = numeroKm * PREZZOKM; 
-console.log(prezzoBase);
-
-// sconto
-
-var prezzoUnder;
-prezzoUnder = prezzoBase - prezzoBase * 20 / 100;
-console.log(prezzoUnder);
-
-var prezzoOver;
-prezzoOver = prezzoBase - prezzoBase * 40 / 100;
-console.log(prezzoOver);
-
-// condizione
+btn_genera.addEventListener('click',
+function () {
  
-if (anni == 18) {
- document.getElementById("costo_biglietto").innerHTML = "Costo biglietto scontato Under18 " + "€" + prezzoUnder.toFixed(2);
-} 
-else if (anni == 65) {
- document.getElementById("costo_biglietto").innerHTML = "Costo biglietto scontato Over65 " + "€" + prezzoOver.toFixed(2);
+ var nome = document.getElementById("nome_cognome").value;
+ console.log(nome);
+
+ var numeroKm = document.getElementById("num_km").value;
+ console.log(numeroKm);
+
+ var anni = document.getElementById("num_anni").value;
+ console.log(anni);
+
+ var offerta = "Biglietto Standard";
+
+ const PREZZOKM = 0.21;
+
+ var prezzoBase;
+ prezzoBase = numeroKm * PREZZOKM; 
+ console.log(prezzoBase);
+
+// // sconto
+
+ var prezzoUnder;
+ prezzoUnder = prezzoBase - prezzoBase * 20 / 100;
+ console.log(prezzoUnder);
+
+ var prezzoOver;
+ prezzoOver = prezzoBase - prezzoBase * 40 / 100;
+ console.log(prezzoOver);
+
+// // condizione
+ 
+ if (anni == 'Minorenne') {
+  document.getElementById("costo_biglietto").innerHTML = "Costo biglietto " + "€" + prezzoUnder.toFixed(2);
+  offerta = "Sconto under 18"
+ } 
+ else if (anni == 'Over 65') {
+  document.getElementById("costo_biglietto").innerHTML = "Costo biglietto " + "€" + prezzoOver.toFixed(2);
+  offerta = "Sconto Silver"
+ }
+ else {
+  document.getElementById("costo_biglietto").innerHTML = "Costo biglietto " + "€" + prezzoBase.toFixed(2);
+ }
+
+ // valori su biglietto
+ document.getElementById("passenger_name").innerHTML = nome;
+ document.getElementById("offerta").innerHTML = offerta;
+ document.getElementById("biglietto").className ="show";
+
+
+ // valori random
+ var carrozza = Math.floor(Math.random() * 9) +1;
+ console.log(carrozza);
+ document.getElementById("carrozza").innerHTML = carrozza;
+ var codiceCp = Math.floor(Math.random()* (100000 - 90000)) + 90000;
+ console.log(codiceCp);
+ document.getElementById("codiceCp").innerHTML = codiceCp;
 }
-else {
- document.getElementById("costo_biglietto").innerHTML = "Costo biglietto Totale " + "€" + prezzoBase.toFixed(2);
-}
+);
+
+// evento collegato bottone annulla
+
+ var btn_annulla = document.getElementById("btn_annulla");
+ btn_annulla.addEventListener('click', 
+ 
+ function reloadPage() {
+  location.reload();
+ }
+ );
